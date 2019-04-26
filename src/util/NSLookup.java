85a67fc015d3ea47ff4e37bb2e8362883b69e316
jo <jo@jo-PC>
2019-04-26 17:18:44 +0900

    박필_NSLookup

---
+++ src/util/NSLookup.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class NSLookup {
+	public static void main(String[] args) {
+		String hostname = "";
+		while(true) {
+			System.out.print("> ");
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+				hostname = br.readLine();
+				if(hostname.equals("exit")) {
+					br.close();
+					System.exit(0);
+				}
+				InetAddress[] inetAddresses = InetAddress.getAllByName(hostname);
+				for(InetAddress addr : inetAddresses) {
+					System.out.println(hostname + " : " + addr.getHostAddress());
+				}
+			} catch (UnknownHostException e) {
+				System.out.println(hostname + "을 찾을 수 없습니다. Non-existent domain");
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
